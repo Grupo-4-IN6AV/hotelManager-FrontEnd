a197dd06e92f8590c3c66612f85346c887fc2550
rcardenas-2020166 <rcardenas-2020166@kinal.edu.gt>
2022-06-23 01:28:48 -0600

    Se agregó el componente de Register.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path: '', component: LandingPageComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
